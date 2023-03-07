1fb26ef0571d9225f73c6c1b7e43fc4a1d992995
Ellen CHan <ellenycc@gmail.com>
2023-03-07 20:43:58 +0000

    Edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true
   validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :nickname, presence: true
   validates :address, presence: true
-  validates :dob, presence: true
+  validates :age, presence: true
   validates :bio, presence: true
   validates :gender, presence: true
 
