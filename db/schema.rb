ea89e52dfa505431fad56b1e37a5c2b0b9e218df
Ellen CHan <ellenycc@gmail.com>
2023-03-08 16:11:31 +0000

    Merge schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_152050) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_05_133830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -36,10 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_152050) do
     t.index ["favoritor_id", "favoritor_type"], name: "fk_favorites"
     t.index ["favoritor_type", "favoritor_id"], name: "index_favorites_on_favoritor"
     t.index ["scope"], name: "index_favorites_on_scope"
-  create_table "chatrooms", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "interests", force: :cascade do |t|
@@ -86,5 +88,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_152050) do
   add_foreign_key "interests", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
-  end
 end
