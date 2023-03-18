8e0580bae9099916a39b30e387728d051c8e0ba4
Bruno Talarico <bruno.talarico@gmail.com>
2023-03-18 16:44:57 +0000

    test
    Signed-off-by: Bruno Talarico <bruno.talarico@gmail.com>

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
-import TomSelect from "tom-select";
 
 // Connects to data-controller="tom-select"
 export default class extends Controller {
   static values = { options: Object }
-  
+
   connect() {
     new TomSelect(
       this.element ,
